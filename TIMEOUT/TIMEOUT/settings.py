6993a4ec57c268a488ec47542df6254e84447f7a
junyeon <wnsdus306@likelion.org>
2019-08-19 19:08:04 +0900

    0819 1908

--- TIMEOUT/TIMEOUT/settings.py
+++ TIMEOUT/TIMEOUT/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-34435345435435
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'mainapp', 'static')
 ]
